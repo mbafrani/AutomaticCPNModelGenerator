bd0e8563a0925ab792aec25f6fef95de1523fadc
Younes Müller <younes.mueller@velocitymobility.com>
2020-11-24 11:53:46 +0100

    Updated Experiments

--- younes experiments.py
+++ younes experiments.py
@@ -7,6 +7,7 @@ from pm4py.algo.enhancement.decision import algorithm as decision_mining
 from pm4py.algo.filtering.log.attributes import (
     attributes_filter as log_attributes_filter,
 )
+import pm4py.visualization.petrinet.common.visualize as vis_petrinet
 
 #%% Import XES
 
@@ -15,16 +16,47 @@ log = xes_importer.apply("input_data/running-example.xes")
 #%% discover and visualize petri net
 net, im, fm = inductive_miner.apply(log)
 
-# dunno what this means
+# Use debug configuration to see the name of added places and skips
 parameters = {visualizer.Variants.WO_DECORATION.value.Parameters.DEBUG: True}
+
 gviz = visualizer.apply(net, im, fm, parameters=parameters,)
 visualizer.view(gviz)
 
 # %% Find information about decision points
-"""Here I discover a decision tree. This is not necessary for the assignment. Rather we need the probabalistic information on decisions."""
 # Decision points are places with more than one outgoing arc
-decision_points = [place for place in net.places if len(place.out_arcs) > 1]
+decision_point_names = list(decision_mining.get_decision_points(net).keys())
+# Get machine learning data from the log.
+decision_frequencies = {}
+for dp_name in decision_point_names:
+    X, y, targets = decision_mining.apply(log, net, im, fm, decision_point=dp_name)
+    prob_info = {}
+    succ_counts = y.value_counts()
+    succ_probs = succ_counts / sum(succ_counts)
+    decision_frequencies[dp_name] = {
+        target: succ_probs[i] for i, target in enumerate(targets)
+    }
+decision_points = [place for place in net.places if place.name in decision_point_names]
+# Enrich petri net
+for dp in decision_points:
+    freq_dict = decision_frequencies[dp.name]
+    dp.properties["successor_frequencies"] = freq_dict
 
+gviz_decorations = {}
+for dp in decision_points:
+    freq_dict = dp.properties["successor_frequencies"]
+    label = "\n".join(name + ": " + f"{freq:.2f}" for name, freq in freq_dict.items())
+
+    gviz_decorations[dp] = {"color": "#b3b6b7 ", "label": label}
+
+gviz = vis_petrinet.apply(
+    net,
+    im,
+    fm,
+    parameters={visualizer.wo_decoration: True},
+    decorations=gviz_decorations,
+)
+visualizer.view(gviz)
+#%%
 for dp in decision_points:
     name = dp.name
     # Find information about the decision point
