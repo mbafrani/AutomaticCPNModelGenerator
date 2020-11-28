507313bf6919db991bb04712d55690fbe1bdacf8
Younes Müller <younes.mueller@velocitymobility.com>
2020-11-28 22:53:44 +0100

    Fix transition frequencies Fixing the bug, where too many successors occured for some decision points.

--- cpn_model_gen/enrich_probability.py
+++ cpn_model_gen/enrich_probability.py
@@ -11,30 +11,50 @@ import pm4py.visualization.petrinet.common.visualize as vis_petrinet
 dict_key = "successor_frequencies"
 
 
-def get_successor_frequencies(log, net, im, fm, dp_names):
+def get_successor_frequencies(log, net, im, fm, dps):
     # Gather information on the log and net.
     I, _ = get_decisions_table(log, net, im, fm,)
 
-    succ_counts = {}
-    for dp in dp_names:
-        succ_list = [el[1] for el in I[dp]]
-        succ_count = Counter(succ_list)
-        succ_sum = sum(succ_count.values())
-        succ_counts[dp] = {name: count / succ_sum for name, count in succ_count.items()}
+    decision_points_succsessor_frequencies = {}
+    for dp in dps:
+        # Get names of transitions that directly follow the decision point
+        successor_names = [arc.target.name for arc in dp.out_arcs]
+        successor_names += [arc.target.label for arc in dp.out_arcs]
 
-    return succ_counts
+        # Get successors to the place from the log and restrict them to the possible transitions
+        successors = [el[1] for el in I[dp.name]]
+        restricted_successors = [succ for succ in successors if succ in successor_names]
+
+        # Calculate Frequencies
+        successor_count = Counter(restricted_successors)
+
+        succ_sum = sum(successor_count.values())
+        for succ in successor_count:
+            successor_count[succ] /= succ_sum
+
+        decision_points_succsessor_frequencies[dp.name] = successor_count
+
+    return decision_points_succsessor_frequencies
 
 
-def enrich_petrinet_prob_dp(log, net, initial_marking, final_marking):
-    dp_names = list(get_decision_points(net).keys())
+def enrich_petrinet_decision_probabilities(log, net, initial_marking, final_marking):
+    decision_points = list(get_decision_points_from_net(net))
     successor_frequencies = get_successor_frequencies(
-        log, net, initial_marking, final_marking, dp_names
+        log, net, initial_marking, final_marking, decision_points
     )
-    for place in net.places:
-        if place.name in dp_names:
-            place.properties[dict_key] = successor_frequencies[place.name]
+
+    # Enrich decision points
+    for dp in decision_points:
+        dp.properties[dict_key] = successor_frequencies[dp.name]
 
     return net
+
+
+def get_decision_points_from_net(net):
+    decision_point_names = list(get_decision_points(net).keys())
+    for decision_point in net.places:
+        if decision_point.name in decision_point_names:
+            yield decision_point
 
 
 def viz_petrinet(net, im, fm):
@@ -44,15 +64,12 @@ def viz_petrinet(net, im, fm):
 
 
 def viz_enriched(net, im, fm):
-    dp_names = list(get_decision_points(net).keys())
+    dps = get_decision_points_from_net(net)
     decorations = {}
-    for dp in net.places:
-        if dp.name not in dp_names:
-            continue
+    for dp in dps:
         freq_dict = dp.properties[dict_key]
-        label = "\n".join(
-            name + ": " + f"{freq:.2f}" for name, freq in freq_dict.items()
-        )
+        strings = [name + ": " + f"{freq:.2f}" for name, freq in freq_dict.items()]
+        label = "\n".join(strings)
         decorations[dp] = {"color": "#b3b6b7 ", "label": label}
     parameters = {visualizer.wo_decoration: True}
     gviz = vis_petrinet.apply(
@@ -62,14 +79,27 @@ def viz_enriched(net, im, fm):
 
 
 if __name__ == "__main__":
-    # log = xes_importer.apply("cpn_model_gen/input_data/running-example.xes")
-    # log = xes_importer.apply("cpn_model_gen/input_data/ETM_Configuration1.xes")
+    # log = xes_importer.apply("[cpn_model_gen/input_data/running-example.xes")
+    # log = xes_importer.apply("[cpn_model_gen/input_data/ETM_Configuration1.xes")
+    # log = xes_importer.apply("cpn_model_gen/input_data/ETM_Configuration2.xes")
 
     # Discover Net
-    log = xes_importer.apply("cpn_model_gen/input_data/ETM_Configuration2.xes")
+    log = xes_importer.apply("cpn_model_gen/input_data/interval_event_log.xes")
     net, init_marking, final_marking = inductive_miner.apply(log)
     viz_petrinet(net, init_marking, final_marking)
 
     # Enrich Net
-    enriched_net = enrich_petrinet_prob_dp(log, net, init_marking, final_marking)
+    enriched_net = enrich_petrinet_decision_probabilities(
+        log, net, init_marking, final_marking
+    )
     viz_enriched(enriched_net, init_marking, final_marking)
+
+    # Print frequencies
+    # for dp in get_decision_points_from_net(net):
+    #     print(dp.name)
+    #     freq_dict = dp.properties[dict_key]
+    #     print(
+    #         "\n\t".join(
+    #             name + ": " + f"{freq:.2f}" for name, freq in freq_dict.items()
+    #         ),
+    #     )
