19ec2cbc920ce3e476a0f9f001df28f7f2d74bf4
Shubham Balyan <sb1851994@gmail.com>
2020-12-05 11:05:11 +0100

    CPN003 added unittests for enrich petri net with performance information

---
+++ source-code/test/enrich_perf_info_test.py
@@ -0,0 +1,57 @@
+import unittest
+
+import os
+
+from pm4py.objects.log.importer.xes import importer as xes_importer
+from pm4py.algo.discovery.inductive import algorithm as inductive_miner
+from pm4py.statistics.sojourn_time.log import get as soj_time_get
+
+from models.enrich_petri_net import EnrichPetriNet
+
+
+def mine_petrinet(path):
+    log = xes_importer.apply(path)
+    net, im, fm = inductive_miner.apply(log)
+    petrinet = EnrichPetriNet(log, net, im, fm)
+    return petrinet, log, net, im, fm
+
+
+base_path = os.path.join(os.path.dirname(__file__), "input_data")
+interval_event_log_path = os.path.join(base_path, "interval_event_log.xes")
+one_timestamp_log_example_path = os.path.join(base_path, "event-log.xes")
+
+
+class TestEnrichPerfInfo(unittest.TestCase):
+    def test_interval_event_log(self):
+        petrinet, log, net, im, fm = mine_petrinet(interval_event_log_path)
+        duration_dict_mean, duration_dict_stdev \
+            = petrinet.get_service_time_two_timestamps(log, parameters={
+                soj_time_get.Parameters.TIMESTAMP_KEY: "time:timestamp",
+                soj_time_get.Parameters.START_TIMESTAMP_KEY: "start_timestamp"})
+        self.assertEqual(len(duration_dict_mean), len(duration_dict_stdev))
+        self.assertEqual(len(duration_dict_mean), 8)
+        self.assertEqual(len(duration_dict_stdev), 8)
+        self.assertIn("place order", duration_dict_mean)
+        self.assertAlmostEqual(duration_dict_mean['place order'], 42.03, 2)
+        self.assertIn("place order", duration_dict_stdev)
+        self.assertAlmostEqual(duration_dict_stdev['place order'], 74.24, 2)
+
+    def test_one_timestamp_event_log(self):
+        petrinet, log, net, im, fm = mine_petrinet(one_timestamp_log_example_path)
+        stats_mean, stats_stdev \
+            = petrinet.get_service_time_single_timestamps(log, net, im, fm)
+        self.assertEqual(len(stats_mean), len(stats_stdev))
+        self.assertEqual(len(stats_mean), 8)
+        self.assertEqual(len(stats_stdev), 8)
+        self.assertIn("decide", stats_mean)
+        self.assertAlmostEqual(stats_mean['decide'], 4564.27, 2)
+        self.assertIn("decide", stats_stdev)
+        self.assertAlmostEqual(stats_stdev['decide'], 3309.835, 3)
+        self.assertIn("register request", stats_mean)
+        self.assertEqual(stats_mean['register request'], 0)
+        self.assertIn("register request", stats_stdev)
+        self.assertEqual(stats_stdev['register request'], 0)
+
+
+if __name__ == "__main__":
+    unittest.main()
--- source-code/test/petri_net_test.py
+++ source-code/test/petri_net_test.py
@@ -2,8 +2,10 @@ import unittest
 
 import os
 
+import pm4py
 from pm4py.objects.log.importer.xes import importer as xes_importer
 from pm4py.objects.log.log import EventLog
+from pm4py.objects.petri.petrinet import PetriNet
 
 from models import PetriNet
 
@@ -24,6 +26,19 @@ class TestPetriNet(unittest.TestCase):
         log_func = obj.import_csv_log(os.path.join("input_data", "event-log.csv"))
         self.assertGreater(len(log_func), 0, "Empty log file")
         self.assertIsInstance(log_func, EventLog, "Not an instance of EventLog")
+
+    def test_discover_process_model(self):
+        log_lib = xes_importer.apply(os.path.join("input_data", "event-log.xes"))
+        obj = PetriNet(log_lib)
+        net, im, fm = obj.discover_process_model()
+        self.assertEqual(len(im), 1)
+        self.assertEqual(len(fm), 1)
+        self.assertIsInstance(net, pm4py.objects.petri.petrinet.PetriNet,
+                              "Not an instance of Petrinet")
+        self.assertGreater(len(net.places), 0, "No places in the petri net")
+        self.assertGreater(len(net.arcs), 0, "No arcs in the petri net")
+        self.assertGreater(len(net._PetriNet__transitions), 0,
+                           "No transitions in the petri net")
 
 
 if __name__ == "__main__":
